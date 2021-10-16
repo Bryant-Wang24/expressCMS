2d8a6c56f4281824f907ef77ab44b477cd96ca5a
Bryant-Wang24 <485969746@qq.com>
2021-10-16 17:06:01 +0800

    生成登陆图形验证码

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,12 +1,21 @@
 const express = require("express")
 const router = express.Router()
+var svgCaptcha = require('svg-captcha');
 
 router.get("/", (req, res) => {
     // res.send("用户登陆页面")
     res.render("admin/login/login.ejs")
 })
+
+
 router.get("/dologin", (req, res) => {
     res.send("执行登陆")
 })
 
+router.get('/verify', function (req, res) {
+    var captcha = svgCaptcha.create();
+    // req.session.captcha = captcha.text;
+    res.type('svg');
+    res.status(200).send(captcha.data);
+});
 module.exports = router
