0832e543d356ee12c35b8d3466f4d1617a1a2e07
Bryant-Wang24 <485969746@qq.com>
2021-10-16 23:00:46 +0800

    后台管理系局部刷新架构

--- routes/admin.js
+++ routes/admin.js
@@ -6,17 +6,20 @@ const login = require("./admin/login")
 const nav = require("./admin/nav")
 const user = require("./admin/user")
 const manager = require('./admin/manager')
+const main = require("./admin/main")
 
 
 
-router.get("/", (req, res) => {
-    res.send("后台管理模块")
-})
+// router.get("/", (req, res) => {
+//     res.render("admin/login/login.ejs")
+// })
 
 // 挂载路由
+router.use('/', main)
 router.use('/login', login)
 router.use('/nav', nav)
 router.use('/manager', manager)
 router.use('/user', user)
+
 
 module.exports = router
