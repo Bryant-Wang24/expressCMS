cc0a2841435956d857aa903efc9987be1c89f54d
Bryant-Wang24 <485969746@qq.com>
2021-10-17 14:46:06 +0800

    权限验证功能

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,22 @@
 const express = require("express")
 const router = express.Router()
+const url = require("url")
+
+// 中间件，权限验证，判断session是否存在
+router.use((req, res, next) => {
+    console.log('权限', req.url);
+    const pathname = url.parse(req.url).pathname
+    if (req.session.userinfo && req.session.userinfo.username) {// session存在
+        next()
+    } else {//session不存在
+        if (pathname == "/login" || pathname == "/login/dologin" || pathname == "/login/verify") {
+            next()//如果是登陆页面,则继续向下执行
+        } else {
+            res.redirect("/admin/login")//如果不是登陆页面,则重定向到登陆页面
+        }
+
+    }
+})
 
 // 引入模块
 const login = require("./admin/login")
