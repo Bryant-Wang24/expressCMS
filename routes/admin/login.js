920c8a417c65f3978414f3659b9f964ed194fb30
Bryant-Wang24 <485969746@qq.com>
2021-10-17 13:47:11 +0800

    登录功能

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -5,23 +5,22 @@ const ManagerModel = require('../../model/managerModel')
 const md5 = require('md5');
 
 router.get("/", async (req, res) => {
-    let result = new ManagerModel({
-        username: "admin",
-        password: md5("123456")
-    })
-    await result.save()
+    // let result = new ManagerModel({
+    //     username: "admin",
+    //     password: md5("123456")
+    // })
+    // await result.save()
     res.render("admin/login/login.ejs")
 })
 
 
-router.post("/dologin", (req, res) => {
-    // const username = req.body.username
-    // const password = req.body.password
+router.post("/dologin", async (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
     const verify = req.body.verify
     console.log(verify, req.session.captcha);
-    // 1、判断用户名密码是否合法
 
-    // 2、判断验证码是否正确
+    // 1、判断验证码是否正确
     if (verify.toLowerCase() !== req.session.captcha.toLowerCase()) {
         // 图形验证码失败
         res.render("admin/public/error.ejs", {
@@ -30,11 +29,24 @@ router.post("/dologin", (req, res) => {
         })
         return
     }
-    // 图形验证码成功
-    res.render("admin/public/success.ejs", {
-        "redirectUrl": "/admin",//成功后跳转的页面
-        "message": "登陆成功"
-    })
+
+    // 2、判断用户名密码是否合法
+    let result = await ManagerModel.find({ "username": username, "password": md5(password) })
+    if (result.length > 0) {
+        // 保存用户信息
+        req.session.userinfo = result[0]
+        // 图形验证码成功
+        res.render("admin/public/success.ejs", {
+            "redirectUrl": "/admin",//成功后跳转的页面
+            "message": "登陆成功"
+        })
+    } else {
+        res.render("admin/public/error.ejs", {
+            "redirectUrl": "/admin/login",//失败后跳转到原页面
+            "message": "用户名或则密码错误"
+        })
+    }
+
 })
 
 router.get('/verify', function (req, res) {
