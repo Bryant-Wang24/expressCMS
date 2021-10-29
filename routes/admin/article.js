68c9251091dfc16856fa3de287be03ee5acfa3d6
Bryant-Wang24 <485969746@qq.com>
2021-10-29 08:49:05 +0800

    富文本编辑器上传图片

--- routes/admin/article.js
+++ routes/admin/article.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var router = express.Router();
+const{multer} = require('../../model/tools')
 
 router.get("/", async (req, res) => {
 
@@ -8,6 +9,11 @@ router.get("/", async (req, res) => {
 router.get("/add", (req, res) => {
     res.render("admin/article/add.ejs")
 })
-
+router.post("/doUploadImage",multer().single('file'),(req,res)=>{
+    const imgSrc = req.file?req.file.path.substr(7):''
+    res.send({
+        link:"/"+imgSrc
+    })
+})
 
 module.exports = router
